fe2d2afcf26534cb2a99ba66ff998043fceb7fa4
Matías Lizarraga <Lizarraga_matias@hotmail.com.ar>
2026-03-29 22:10:47 -0300

    refactor: rename base package to taskly

---
+++ src/main/java/com/matias/taskly/TasklyApiApplication.java
@@ -0,0 +1,15 @@
+package com.matias.taskly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklyApiApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/matias/taskly/TasklyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.taskly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
